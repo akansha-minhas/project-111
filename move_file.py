587fea544a7ed09039b1a758fb00e82307d1e739
Akki <87625749+akansha-minhas@users.noreply.github.com>
2022-10-09 13:14:24 -0600

    Add files via upload

---
+++ move_file.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+from_dir = "C:/Users/Administrator/Downloads"
+to_dir = "C:/Users/Administrator/OneDrive/Desktop/Akansha's Coding Projects/document_files"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
